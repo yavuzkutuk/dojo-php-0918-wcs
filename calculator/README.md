c7a2d40440cab9f5c02dd17ab45553837dd8fcb5
Aurélien WCS <aurelien@wildcodeschool.fr>
2018-10-22 11:36:22 +0200

    style calculator ok

---
+++ calculator/README.md
@@ -0,0 +1,5 @@
+# Calculator
+
+## Énoncé
+
+A partir du template index.php déjà présent, crée une calculatrice
---
+++ calculator/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<form class="main" method="post">
+    <input type="text" class="screen">
+    <section id="controls">
+        <button id="clear">C</button>
+        <button id="divide">&divide;</button>
+        <button id="multiply">&times;</button>
+        <button id="number-7">7</button>
+        <button id="number-8">8</button>
+        <button id="number-9">9</button>
+        <button id="subtract">-</button>
+        <button id="number-4">4</button>
+        <button id="number-5">5</button>
+        <button id="number-6">6</button>
+        <button id="add">+</button>
+        <button id="number-1">1</button>
+        <button id="number-2">2</button>
+        <button id="number-3">3</button>
+        <button id="number-0">0</button>
+        <button id="decimal">.</button>
+        <button type="submit" id="equals">=</button>
+    </section>
+</form>
+<script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+</body>
+</html>
