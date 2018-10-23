28a7360515dac99c89b48dc26fcf079de0de17c4
Aurélien <AurelienBarbier@users.noreply.github.com>
2018-10-23 18:09:25 +0200

    Update hamming.php

--- hamming/hamming.php
+++ hamming/hamming.php
@@ -1,1 +1,12 @@
 <?php
+
+function dna(string $a, string $b) :int {
+    $counting = 0;
+    for($i = 0; $i < strlen($a); $i++) {
+        if($a[$i] !== $b[$i]) {
+            $counting +=1;
+        }
+    }
+}
+
+echo dna('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCT');
